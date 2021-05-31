ed9495f0cc00083e606dca0c9b41575af781ba94
1m3a <84995778+1m3a@users.noreply.github.com>
2021-05-31 16:52:16 +0300

    Delete Ecommerce-Nodejs-master directory

--- Ecommerce-Nodejs-master/configs/jwt-config.js
+++
@@ -1,4 +0,0 @@
-//use your secret
-module.exports = {
-  secret: "This is a secret between me and Mustafa the front-end project developer So HEEEEEEYEAH",
-}
--- Ecommerce-Nodejs-master/configs/mongo-config.js
+++
@@ -1,7 +0,0 @@
-// sakete8120@brayy.com
-const password = "KgoxKGlaaM5bOrlb";
-
-// export mongodb url
-const url = `mongodb+srv://sakete8120:${password}@cluster0.vhu8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-module.exports = url;
